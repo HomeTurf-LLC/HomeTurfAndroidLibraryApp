50dda03d083cc534e83912b61e10a8efc6e6e22f
Marcela Rodriguez <arilaen@gmail.com>
2021-04-15 10:14:05 -0400

    remove hometurf library (will be brought in as submodule)

--- hometurf/src/main/java/com/hometurf/android/constants/PermissionCodes.java
+++
@@ -1,8 +0,0 @@
-package com.hometurf.android.constants;
-
-public class PermissionCodes {
-    public static final int REQUEST_FINE_LOCATION = 0;
-    public static final int MY_PERMISSIONS_RECORD_AUDIO = 1;
-    public static final int INPUT_FILE_REQUEST_CODE = 2;
-    public static final int REQUEST_CAMERA_FOR_UPLOAD = 3;
-}
